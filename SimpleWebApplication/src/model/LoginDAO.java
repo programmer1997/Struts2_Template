6c2b7c7bebf79f28bfc5eddad76856468e42851b
programmer1997 <ajinkya001@e.ntu.edu.sg>
2018-10-28 11:14:18 +0800

    Added different fields for username, password, url

--- SimpleWebApplication/src/model/LoginDAO.java
+++ SimpleWebApplication/src/model/LoginDAO.java
@@ -7,12 +7,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginDAO {
-	
+
+			  private static  String database_url="jdbc:mysql://localhost:3306/CZ3002";
+			  private  static String database_username="root";
+			  private static String database_password="password";
 	public  static boolean validate(String username,String password){
 		 try{
 	            // connecting to the database
 	            Class.forName("com.mysql.jdbc.Driver").newInstance();
-	            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CZ3002", "root", "CZ4031admin");
+	            Connection connection = DriverManager.getConnection(database_url,database_username,database_password);
 	            String query="select username,pass from login";
 	            PreparedStatement p;
 	            p=connection.prepareStatement(query);
@@ -20,33 +23,33 @@ public class LoginDAO {
 	            rs.next();
 	            String db_username=rs.getString("username");
 	            String db_password=rs.getString("pass");
-	            
-	            // validating 
+
+	            // validating
 				if ((username.equals(db_username))&&(password.equals(db_password))) return true;
 				else return false;
-		 
+
 		 }
 		 catch(IllegalAccessException iae){
-			 
+
 		 }
 		 catch(InstantiationException i){
-			 
+
 		 }
 		 catch(SQLException se){
-			 
+
 		 }
 		 catch(ClassNotFoundException cnfe){
-			 
+
 		 }
-		 
+
 		 return false;
-	
-           
-			
-		
+
+
+
+
 	}
-		
-		 
+
+
 
 
 }
